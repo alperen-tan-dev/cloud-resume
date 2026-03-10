44ed9428648c35349870ab121b1c2a17baa0c043
Alperen <alperentan08@gmail.com>
2026-03-10 15:54:22 +0300

    feat: integrated terraform and s3 deploy

---
+++ terraform/main.tf
@@ -0,0 +1,89 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-north-1"
+}
+
+resource "aws_s3_bucket" "cv_bucket" {
+  bucket = "alperen-bulut-cv-2026"
+}
+
+resource "aws_cloudfront_distribution" "cv_distribution" {
+  origin {
+    domain_name = "alperen-bulut-cv-2026.s3.eu-north-1.amazonaws.com"
+    origin_id   = "alperen-bulut-cv-2026.s3.eu-north-1.amazonaws.com-mmjbhkunp7m"
+
+    s3_origin_config {
+      origin_access_identity = ""
+    }
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "alperen-bulut-cv-2026.s3.eu-north-1.amazonaws.com-mmjbhkunp7m"
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+
+  lifecycle {
+    ignore_changes = [origin, default_cache_behavior]
+  }
+
+  tags = {
+    Name = "alperen-cv-proxy"
+  }
+}
+
+resource "aws_dynamodb_table" "visitor_counter" {
+  name         = "cloud-resume-stats"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+}
+
+resource "aws_lambda_function" "cv_lambda" {
+  function_name = "resume-counter-func"
+  role          = "arn:aws:iam::859217211762:role/service-role/resume-counter-func-role-5tzfz7zi"
+  handler       = "lambda_function.lambda_handler"
+  runtime       = "python3.12"
+  filename      = "lambda_function.zip"
+
+
+  source_code_hash = filebase64sha256("lambda_function.zip")
+}
