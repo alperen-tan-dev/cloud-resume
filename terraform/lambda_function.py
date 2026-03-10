0623f1c2a7af3a7bdf33211c3a3b58095e817073
Alperen <alperentan08@gmail.com>
2026-03-10 18:22:21 +0300

    force: updating lambda code with new zip name

--- terraform/lambda_function.py
+++ terraform/lambda_function.py
@@ -5,15 +5,13 @@ dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table('cloud-resume-stats')
 
 def lambda_handler(event, context):
-    # Veritabanından sayıyı almayı dene
+    
     response = table.get_item(Key={'id': '0'})
     item = response.get('Item')
     
-    # Eğer tabloda veri yoksa 0'dan başla, varsa olanı al
     count = item['count'] if item else 0
     new_count = count + 1
     
-    # Yeni sayıyı kaydet
     table.put_item(Item={'id': '0', 'count': new_count})
     
     return {
